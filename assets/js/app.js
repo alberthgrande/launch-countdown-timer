9e4c91cc5885b8abf4b31359a4d161227b220458
alberthgrande <alberthgrande98@gmail.com>
2023-03-28 22:59:03 +0800

    current update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
-console.log("hello");
+const day = document.querySelector(".current-time__days");
+const hour = document.querySelector(".current-time__hours");
+const minute = document.querySelector(".current-time__minutes");
+const second = document.querySelector(".current-time__seconds");
+
+// Set the date we're counting down to
+const countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function () {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 500);
